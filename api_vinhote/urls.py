f12a904c03dbb4c0ba56fe872d2ed3aa88650aa8
Thiago Vinhote <thiagovf26@outlook.com>
2018-03-07 20:57:40 -0400

    Redirect to api/v1
    Signed-off-by: Thiago Vinhote <thiagovf26@outlook.com>

--- api_vinhote/urls.py
+++ api_vinhote/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+  path('', RedirectView.as_view(url = 'api/v1')),
   path('admin/', admin.site.urls),
-
   path('api/v1/', include('api.urls')),
 ]
